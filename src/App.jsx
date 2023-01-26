370a064343d5c6c0230b455ec0a9f648278dbc43
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-27 03:26:12 +0330

    import toast from react-hot-toast and add style to code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import toast, { Toaster } from "react-hot-toast";
+
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <div>
+    <div className=" bg-slate-800 h-screen  w-full">
+      <div className="  text-center max-w-[900px]   h-full m-auto p-4">
+        <Toaster position="top-right" />
+        <h1 className="text-white  text-3xl mb-9">Todo List App</h1>
         <TodoForm />
         <TodoList />
       </div>
