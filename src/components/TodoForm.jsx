2aedb203c30c5946501eecbf42077008cf9c7d6f
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-27 03:26:52 +0330

    add style and improve the logic of code

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 const TodoForm = () => {
   return (
-    <form>
-      <div>
-        <input type="text" placeholder="Add Todo..." />
-        <div>
-          <button>Update</button>
-          <button>Cancel</button>
-        </div>
+    <form className="flex items-center  ">
+      <div className="w-full  ">
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          className=" outline-none p-2 rounded-md md:w-[80%] "
+        />
+
+        <button type="submit" className="bg-red-300 p-2 rounded-md w-auto ml-2">
+          Add Task
+        </button>
       </div>
     </form>
   );
