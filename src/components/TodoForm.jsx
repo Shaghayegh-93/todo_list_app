811c2d0b59fa07a3ef878e38fce80c66893925b9
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-28 19:28:13 +0330

    improve style of component

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const TodoForm = () => {
   return (
-    <form className="flex items-center  ">
-      <div className="w-full  ">
+    <form className="">
+      <div className=" grid grid-cols-4 md:w-[90%]  m-auto  mb-4">
         <input
           type="text"
           placeholder="Add Todo..."
-          className=" outline-none p-2 rounded-md md:w-[80%] "
+          className=" outline-none p-2 rounded-md col-span-3  "
         />
 
-        <button type="submit" className="bg-red-300 p-2 rounded-md w-auto ml-2">
+        <button type="submit" className="bg-red-300 p-2 rounded-md w-auto ml-4 col-span-1 ">
           Add Task
         </button>
       </div>
