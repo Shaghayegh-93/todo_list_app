4e68eda77472b2606c0ac63e0ccfd086166c7051
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-28 19:28:51 +0330

    import and render Todo component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Todo from "./Todo";
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return <div className="">
+    <Todo/>
+  </div>;
 };
 
 export default TodoList;
