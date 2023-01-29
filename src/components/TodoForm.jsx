b5433fdb1a43dbed699fe07f2a20221582aa2d07
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-29 21:20:09 +0330

    expand the code and add state for input and add function for submit the form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodoHandler }) => {
+  const [todoInput, setTodoInput] = useState("");
+  const changeHandler = (e) => {
+    setTodoInput(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!todoInput) {
+      toast.apply("hiiii");
+      return;
+    }
+    addTodoHandler(todoInput);
+    setTodoInput("");
+  };
   return (
-    <form className="">
+    <form onSubmit={submitHandler} className="">
       <div className=" grid grid-cols-4 md:w-[90%]  m-auto  mb-4">
         <input
           type="text"
           placeholder="Add Todo..."
           className=" outline-none p-2 rounded-md col-span-3  "
+          value={todoInput}
+          onChange={changeHandler}
         />
 
-        <button type="submit" className="bg-red-300 p-2 rounded-md w-auto ml-4 col-span-1 ">
+        <button
+          type="submit"
+          className="bg-green-700 p-2 rounded-md w-auto ml-4 col-span-1 text-white "
+        >
           Add Task
         </button>
       </div>
