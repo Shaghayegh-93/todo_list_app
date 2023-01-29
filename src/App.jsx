2e4de1f0728181692fcb72a1b352f4eb3d22cabe
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-30 00:08:04 +0330

    add function for delete every todo

--- src/App.jsx
+++ src/App.jsx
@@ -26,13 +26,22 @@ function App() {
     setTodoList(updatedTodo);
   };
 
+  const onDelete = (id) => {
+    const filteredTodo = todoList.filter((todo) => todo.id !== id);
+    setTodoList(filteredTodo);
+  };
+
   return (
     <div className=" bg-slate-800 h-screen  w-full">
       <div className="  text-center max-w-[900px]   h-full m-auto p-4">
         <Toaster position="top-right" />
         <h1 className="text-white  text-3xl mb-9">Todo List App</h1>
         <TodoForm addTodoHandler={addTodoHandler} />
-        <TodoList todoList={todoList} toggleTodo={toggleTodo} />
+        <TodoList
+          todoList={todoList}
+          toggleTodo={toggleTodo}
+          onDelete={onDelete}
+        />
       </div>
     </div>
   );
