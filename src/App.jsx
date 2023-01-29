59734d9fa61246abe8110615b54aca63073624de
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-29 21:17:19 +0330

    add state for all todos and add function for adding todo and send props to TodoForm and TodoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import "./App.css";
-import toast, { Toaster } from "react-hot-toast";
-
+import { Toaster } from "react-hot-toast";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import { useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodoHandler = (todo) => {
+    const newTodo = { text: todo, id: Math.floor(Math.random() * 1000) };
+
+    setTodoList([...todoList, newTodo]);
+    console.log("todoList:", todoList);
+  };
+
   return (
     <div className=" bg-slate-800 h-screen  w-full">
       <div className="  text-center max-w-[900px]   h-full m-auto p-4">
         <Toaster position="top-right" />
         <h1 className="text-white  text-3xl mb-9">Todo List App</h1>
-        <TodoForm />
-        <TodoList />
+        <TodoForm addTodoHandler={addTodoHandler} />
+        <TodoList todoList={todoList} />
       </div>
     </div>
   );
