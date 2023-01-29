57e07b6639d4ef2348351eca704c40c33f192d83
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-29 23:45:20 +0330

    recieve and send ontoggleTodo as props

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({ todoList }) => {
-  console.log(todoList)
+const TodoList = ({ todoList, toggleTodo }) => {
+  console.log(todoList);
   return (
     <div className="">
-      {todoList.map((todo,index) => {
-        return <Todo  todo={todo} key={todo.id} index={index} />;
+      {todoList.map((todo, index) => {
+        return (
+          <Todo
+            todo={todo}
+            key={todo.id}
+            index={index}
+            onToggleTodo={()=>toggleTodo(todo.id)}
+          />
+        );
       })}
     </div>
   );
