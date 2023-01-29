c812fdcf35cf2bfaff18a14e32e04aa727bede9e
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-01-29 21:18:21 +0330

    improve the logic of code

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = () => {
-  return <div className="">
-    <Todo/>
-  </div>;
+const TodoList = ({ todoList }) => {
+  console.log(todoList)
+  return (
+    <div className="">
+      {todoList.map((todo,index) => {
+        return <Todo  todo={todo} key={todo.id} index={index} />;
+      })}
+    </div>
+  );
 };
 
 export default TodoList;
