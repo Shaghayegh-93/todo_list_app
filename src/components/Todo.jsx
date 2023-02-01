dd9c0962a127964e44d5f4b8768c3e94dd499d14
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-01 23:30:51 +0330

    send onEdit as props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,7 @@ import { BsFillCheckCircleFill } from "react-icons/bs";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { MdModeEditOutline } from "react-icons/md";
 
-const Todo = ({ todo, index, onToggleTodo, onDelete }) => {
+const Todo = ({ todo, index, onToggleTodo, onDelete, onEdit }) => {
   return (
     <div className="flex flex-col items-center my-2">
       <div className="bg-slate-600 p-2 w-full md:w-[90%] rounded-md text-white flex items-center justify-between">
@@ -28,9 +28,7 @@ const Todo = ({ todo, index, onToggleTodo, onDelete }) => {
           ) : (
             ""
           )}
-          <MdModeEditOutline
-            className="mr-1 ml-2"
-          />
+          <MdModeEditOutline className="mr-1 ml-2" onClick={onEdit} />
           <RiDeleteBin5Fill className="text-red-600 " onClick={onDelete} />
         </div>
       </div>
