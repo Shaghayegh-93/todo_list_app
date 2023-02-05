dbcb822f7721123a987115987e2cfd3d467a4f51
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-06 00:04:04 +0330

    again transfer to jsx format

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = ({
+  todoList,
+  toggleTodo,
+  onDelete,
+  setEdit,
+}) => {
+
+  return (
+    <div className="">
+      {todoList.map((todo, index) => {
+        return (
+          <Todo
+            todo={todo}
+            key={todo.id}
+            index={index}
+            onToggleTodo={() => toggleTodo(todo.id)}
+            onDelete={() => onDelete(todo.id)}
+            onEdit={() => setEdit(todo)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
