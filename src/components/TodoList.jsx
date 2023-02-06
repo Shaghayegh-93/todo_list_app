2ecd38d6af899afdc5019bbd1840fed7db089ea1
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-06 22:27:07 +0330

    improve the logic of code

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({
-  todoList,
-  toggleTodo,
-  onDelete,
-  setEdit,
-}) => {
+const TodoList = ({ todoList, toggleTodo, onDelete, setEdit }) => {
+  const renderTodo = () => {
+    if (todoList.length === 0) return <div className="text-white">No tasks</div>;
+    return (
+      <div className="task-list">
+        {todoList.map((todo, index) => {
+          return (
+            <Todo
+              todo={todo}
+              key={todo.id}
+              index={index}
+              onToggleTodo={() => toggleTodo(todo.id)}
+              onDelete={() => onDelete(todo.id)}
+              onEdit={() => setEdit(todo)}
+            />
+          );
+        })}
+      </div>
+    );
+  };
 
-  return (
-    <div className="">
-      {todoList.map((todo, index) => {
-        return (
-          <Todo
-            todo={todo}
-            key={todo.id}
-            index={index}
-            onToggleTodo={() => toggleTodo(todo.id)}
-            onDelete={() => onDelete(todo.id)}
-            onEdit={() => setEdit(todo)}
-          />
-        );
-      })}
-    </div>
-  );
+  return <div className="">{renderTodo()}</div>;
 };
 
 export default TodoList;
